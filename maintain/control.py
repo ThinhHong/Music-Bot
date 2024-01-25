a71faee00a35eabb75405df6340a2bea7527546c
ThinhHong <thinhhong1998@gmail.com>
2024-01-24 23:28:28 -0500

    Updated control.py
    Added function to allower admins to set the welcome message

--- maintain/control.py
+++ maintain/control.py
@@ -5,18 +5,27 @@ from configuration import config
 
 class Control(commands.Cog):
 
+    
     def __init__(self, bot):
         self.bot = bot
+        self.welcome_message = "Welcome to our server"
     
     @commands.event
     async def on_member_join(self, member):
         channel = self.bot.get_channel(config.BOT_CHANNEL)
-        await channel.send(f"Welcome {member} to our server")
+        await channel.send(f"{self.welcome_message} {member}")
 
     @commands.event
     async def on_member_remove(self,member):
         channel = self.bot.get_channel(config.BOT_CHANNEL)
         await channel.send(f"{member} has left")
+
+    @commands.event
+    async def set_welcome_message(self, ctx):
+        self.welcome_message = ctx 
+        channel = self.bot.get_channel(config.BOT_CHANNEL)
+        await channel.send(f"{self.welcome_message} is now welcome message")
+
 
 async def setup(bot):
     await bot.add_cog(Control(bot))
