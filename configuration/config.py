72e1fbd47cf9afec9b5d1d7c098b81053a7f5b57
ThinhHong <thinhhong1998@gmail.com>
2024-02-14 00:01:08 -0500

    Added config descriptions
    Added several help descriptions to help users use function.

--- configuration/config.py
+++ configuration/config.py
@@ -2,4 +2,6 @@
 HELP_ON_READY = "Functions activates when a user joins the discord/guild"
 BOT_DESCRIPTION = "This bot is designed to play music in any discord channel. As well as queueing and playlist"
 HELP_JOIN = "Command joins bot into the voice channel the user is currently in. Will not work unless user is in channel."
+HELP_LEAVE = "Bot wil leave voice channnel it is currently in. Fails it not in call"
+HELP_PLAY = "Command will play input as song File needs to be in music bot folder"
 BOT_CHANNEL = 1191775942553649362
